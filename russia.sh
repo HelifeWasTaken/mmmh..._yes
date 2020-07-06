047c2278aa5638f31cee15133b3c0123c4a4e9f4
Mattis DALLEAU <mattisdalleau@gmail.com>
2020-07-07 02:28:06 +0400

    Update russia.sh

--- russia.sh
+++ russia.sh
@@ -1,7 +1,7 @@
 setxkbmap ru
 notify-send -u critical -i ~/.trollscript/russia.jpg "Votre ordinateur est maintenant NOTRE ordinateur"
 etxkbmap rufplay -nodisp -autoexit ~/.trollscript/russia.mp3 1>&2 2> /dev/null&
-do
+while true; do
     amixer set Master 100% > /dev/null
     sleep 0.5
 done
